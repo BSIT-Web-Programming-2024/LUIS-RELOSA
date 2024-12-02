73a2bbc34dc7426e069040fb869f08a37def51fd
= <=>
2024-12-02 23:40:32 +0800

    commit

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-document.getElementById('btn').addEventListener('click', function () {
-    const image = document.getElementById('loptop');
-    
-    // Enlarge the image
-    image.classList.add('enlarged');
-    
-    // Wait for the animation to complete before redirecting
-    setTimeout(() => {
-        window.location.href = 'desktop.html'; // Change this to your desired URL
-    }, 250); // Match the delay to your CSS transition time (0.5s = 500ms)
-});
+const home = document.getElementById('home');
+const model1 = document.getElementById('model1');
+const me = document.getElementById('me');
+const achievement = document.getElementById('achievement');
+const education = document.getElementById('education');
+const skills = document.getElementById('skills');
+const hobbies = document.getElementById('hobbies');
+
+const baseline = window.innerHeight * 0.8;
+
+
+window.addEventListener('scroll',function(){
+
+const value = window.scrollY;
+
+home.style.marginLeft= value * 1.5 + 'px';
+model1.style.marginLeft= value * 1.6 + 'px';
+me.style.marginLeft = value * 2 + 'px';
+achievement.style.right = value * 1.2 + 'px';
+education.style.marginLeft = value * 2 + 'px';
+skills.style.right= value * 1.5 + 'px';
+hobbies.style.marginLeft= value + 1.5 + 'px';
+
+})
